a502d0c8552f3dcaf410fc8cb64664f6aed92660
cho chan young <whcksdud8@gmail.com>
2022-09-03 06:48:48 +0900

    practice1

--- practice3.py
+++ practice3.py
@@ -1,7 +1,7 @@
 def profile(name,age,*language):
-    print("성명:{}\t 나이:{}\t ".format(name,age),end=" ")
+    print("이름:{0}\t나이:{1} \t".format(name,age), end=" ")
     for lang in language:
         print(lang,end=" ")
     print()
-profile("거북이", 100 , "영어","프랑스어","독일어")
-profile("황소",10,"한국어","일본어","중국어","힌두어","영어")
+
+profile("하마","20","한국어","독일어","영어","일본어")
