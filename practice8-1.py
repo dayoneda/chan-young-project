fe574125d05d50cea407f0bd700ac48678116a37
cho chan young <whcksdud8@gmail.com>
2022-09-04 07:06:52 +0900

    0904

---
+++ practice8-1.py
@@ -0,0 +1,8 @@
+#grade = {"수학": 50, "영어": 25, "코딩": 100}
+#for key, value in grade.items():
+    #print(key, value)
+    #print(key.ljust(2),str(value).rjust(4), sep=":")
+
+#은행 대기순번표
+for num in range(1,21):
+    print("대기번호:"+ str(num).zfill(3))
---
+++ practice8-2.py
@@ -0,0 +1,35 @@
+
+
+
+
+
+#print("{0:^<10,}".format(3000))
+#print("{0: >20}".format(300))
+#print("{0:^>+30,}".format(500000000000000))
+#print("{0:.4f}".format(5/3))
+#for i in range(5):
+ #   print("{0:<5}".format("*"*(i+1)))
+
+#for i in range(5,0,-1):
+ #   print("{0:<5}".format("*"*i))
+
+for i in range(5):
+    print("{0:>5}".format("*"*(i+1)))
+
+for i in range(5,0,-1):
+    print("{0:<5}".format("*"*i))
+
+
+
+for i in range(1,11,2):
+    print("{0:^10}".format("*"*i))
+
+for i in range(9,0,-2):
+    print("{0:^10}".format("*"*i))
+
+for i in range(9,0,-2):
+    print("{0:^10}".format("*"*i))
+
+for i in range(3,11,2):
+    print("{0:^10}".format("*"*i))
+
