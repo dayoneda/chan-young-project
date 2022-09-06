54073c3ff5de0fde5e70770ad24d35ab03ed19ea
cho chan young <whcksdud8@gmail.com>
2022-09-06 23:50:04 +0900

    0906

--- practice 9-1.py
+++ practice 9-1.py
@@ -14,7 +14,19 @@ class AttackUnit(Unit):
         self.hp-=damaged
         if self.hp<=0:
             print("{0}유닛이 파괴되었습니다".format(self.name))
-magician=AttackUnit("magician",200,9999)
-magician.attack(1)
-magician.damaged(40)
-magician.damaged(500)
+class flyable:
+    def __init__(self,flying_speed):
+        self.flying_speed=flying_speed
+    def fly(self,name,location):
+        print("{0}유닛이 {1}방향으로 이동중입니다 이동속도:{2}"\
+            .format(name,location,self.flying_speed))    
+
+class flyableAttackUnit(AttackUnit,flyable):
+    def __init__(self,name,hp,damage,flying_speed):
+        AttackUnit.__init__(self,name,hp,damage)
+        flyable.__init__(self,flying_speed)
+
+
+valkyrie=flyableAttackUnit("발키리",250,12,25)
+valkyrie.attack(2)
+valkyrie.fly("발키리",2)
