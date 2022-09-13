06cd5e9a5532c5d66f8bf152b11bb049eba3f6c7
cho chan young <whcksdud8@gmail.com>
2022-09-13 09:42:26 +0900

    0913

--- practice10-1.py
+++ practice10-1.py
@@ -1,8 +1,21 @@
+class BigNumberError(Exception):
+    def __init__(self,msg):
+        self.msg=msg
+    def __str__(self):
+        return self.msg
+
 try:
-    num1,num2=map(int,input("계산하는 숫자를 입력해주세요 ").split())
-    num3=round(num1/num2,2)
-    print("{}/{}={}입니다".format(num1,num2,num3))
+    print("한 자리수 전용계산기입니다")
+    num1,num2=map(int,input("값을 입력해주세요 ").split())
+    if num1>=10 or num2>=10:
+        raise BigNumberError("입력값:{0},{1}".format\
+            (num1,num2))
+    print("{1}/{2}={3}입니다".format(num1,num2,num1/num2))
+    
 except ValueError:
-    print("숫자를 입력해주세요")
-except ZeroDivisionError as err:
+    print("잘못된 값을 입력하였습니다")
+except BigNumberError as err:
+    print("에러가 발생하였습니다")
     print(err)
+finally:
+    print("이용해주셔서 감사합니다")
